e39bbf7d16539a780a6f445e48a0d345daddc375
ameerthehacker <ameerjhanprof@gmail.com>
2017-11-06 12:53:29 +0530

    Add gulp task to optimize images and edit wildcard for sass task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const minifyCSS = require('gulp-minify-css');
 const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
 
 // Source directory
 const srcDir = './src';
@@ -13,7 +14,7 @@ const distDir = './dist';
  Gulp task to convert sass to vanills css
 */
 gulp.task('sass', () => {
-    gulp.src(`${srcDir}/sass/*.scss`)
+    gulp.src(`${srcDir}/sass/**/*.scss`)
         .pipe(sass())
         .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
         .pipe(minifyCSS())
@@ -22,8 +23,18 @@ gulp.task('sass', () => {
 });
 
 /*
+ Gulp task to optimize images
+*/
+gulp.task('imagemin', () => {
+    gulp.src([`${srcDir}/images/**/*.png`, `${srcDir}/images/**/*.jpg`, `${srcDir}/images/**/*.jpeg`])
+        .pipe(imagemin())
+        .pipe(gulp.dest(`${distDir}/images`));
+});
+
+/*
 Gulp task to watch the changes in the src directory
 */
 gulp.task('watch', () => {
     gulp.watch(`${srcDir}/sass/*.scss`, ['sass']);
+    gulp.watch([`${srcDir}/images/**/*.png`, `${srcDir}/images/**/*.jpg`, `${srcDir}/images/**/*.jpeg`], ['imagemin']);    
 });
